43f9d203aeee6bac81c1d6b429aeb753f6733248
Antoine Pultier <antoine@blogjaune.fr>
2012-03-13 20:40:47 +0100

    message admin

--- View/DashboardView.php
+++ View/DashboardView.php
@@ -1,19 +1,28 @@
 <?php
 class DashboardView {
 	public static function showInfosCredit($credit) {
-	$credit = htmlspecialchars($credit);
-	$nom = htmlspecialchars($_SESSION['user']->name);
-	echo <<<END
+		$credit = htmlspecialchars($credit);
+		$nom = htmlspecialchars($_SESSION['user']->name);
+		echo <<<END
 	<div class="well">
 	<h3>
 	Bonjour $nom.
 	</h3>
 	<hr/>
+END;
+		if ($_SESSION['user']->isAdmin)
+		{
+			echo '<p>Vous êtes administrateur du site.</p>';	
+		}
+		else
+		{
+			echo <<<END
 	<a href="#" class="btn btn-success float_right">Rajouter du crédit</a>
 	<p> Vous avez <strong>$credit</strong> € de crédit.</p>
 
 	</div>
 END;
+		}
 	}
 	
 	public static function showListTitle() {
